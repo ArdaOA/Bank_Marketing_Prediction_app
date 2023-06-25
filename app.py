fec29e191c8215199e15176e6394c60dfa47e322
ArdaOA <119882740+ArdaOA@users.noreply.github.com>
2023-06-25 14:56:10 +0300

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
 import joblib
 
 model = joblib.load('/Users/ardaorkunaydin/Desktop/TEDU/ADS-542/bank_marketing.joblib')
