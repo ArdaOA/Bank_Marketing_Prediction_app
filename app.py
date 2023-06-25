936ae61a16d74785117d9a9db94bcdbe4f9ee937
ArdaOA <119882740+ArdaOA@users.noreply.github.com>
2023-06-25 12:56:27 +0300

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-model = pickle.load(open('/Users/ardaorkunaydin/Desktop/TEDU/ADS-542/bank_marketing_predictor.pkl', 'wb'))
+model = joblib.load('/Users/ardaorkunaydin/Desktop/TEDU/ADS-542/bank_marketing.joblib')
 
 def predict(model, input_df):
     predictions_df = model.predict(input_df)
