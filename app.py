dabac08ea0c0d5ff4bcfc35ee0cc22316a8a1ba8
ArdaOA <119882740+ArdaOA@users.noreply.github.com>
2023-06-25 13:53:08 +0300

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import joblib
 
-model = joblib.load('bank_marketing.joblib')
+model = joblib.load('/Users/ardaorkunaydin/Desktop/TEDU/ADS-542/bank_marketing')
 
 def predict(model, input_df):
     predictions_df = model.predict(input_df)
